dcda5273a11930c1a985c6efb2b2efb46179fa67
BWRose <sonq.ovanesian@gmail.com>
2026-04-14 23:07:20 +0300

    Removed appearence components artifacts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,13 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"  @class(['dark' => ($appearance ?? 'system') == 'dark'])>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- Inline style to set the HTML background color based on our theme in app.css --}}
+        {{-- Keep the app in light mode. --}}
         <style>
             html {
                 background-color: oklch(1 0 0);
-            }
-
-            html.dark {
-                background-color: oklch(0.145 0 0);
             }
         </style>
 
